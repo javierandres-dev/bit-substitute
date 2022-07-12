99565a04bae5fb0eeccd9a0958a32ff530b444fc
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-07-11 22:07:26 -0500

    crud users

--- 01/src/routes/users.js
+++ 01/src/routes/users.js
@@ -5,24 +5,76 @@ const userSchema = require('../models/users');
 /* Create-Read-Update-Delete */
 // Create
 router.post('/users', (request, response) => {
-  response.send('crear usuario...');
-  console.log(request.body);
-  //const newUser =
+  //response.send('crear usuario...');
+  //console.log(request.body);
+  const newUser = userSchema(request.body);
+  /*
+  Promesa:
+  1. Pendiente
+  2. Si cumplió => then
+  3. No cumplió => catch
+  */
+  newUser
+    .save()
+    .then((data) => {
+      response.json({ success: data });
+    })
+    .catch((error) => {
+      response.json({ failured: error });
+    });
 });
 // Read
 router.get('/users', (request, response) => {
-  response.send('Leer todos los usuarios...');
+  //response.send('Leer todos los usuarios...');
+  userSchema
+    .find()
+    .then((data) => {
+      response.json({ success: data });
+    })
+    .catch((error) => {
+      response.json({ failured: error });
+    });
 });
 router.get('/users/:id', (request, response) => {
-  response.send('Leer un usuario específico.');
+  //response.send('Leer un usuario específico.');
+  const { id } = request.params;
+  userSchema
+    .findById(id)
+    .then((data) => {
+      response.json({ success: data });
+    })
+    .catch((error) => {
+      response.json({ failured: error });
+    });
 });
 // Update
 router.put('/users/:id', (request, response) => {
-  response.send('Actualizar un usuario específico.');
+  //response.send('Actualizar un usuario específico.');
+  const { id } = request.params;
+  console.log('id:', id);
+  const user = userSchema(request.body);
+  console.log('user:', user);
+  userSchema
+    .updateOne({ _id: id }, { $set: user })
+    .then((data) => {
+      response.json({ success: data });
+    })
+    .catch((error) => {
+      response.json({ failured: error });
+    });
 });
 // Delete
 router.delete('/users/:id', (request, response) => {
-  response.send('Eliminar un usuario específico.');
+  //response.send('Eliminar un usuario específico.');
+  const { id } = request.params;
+  userSchema
+    .deleteOne({ _id: id })
+    .then((data) => {
+      response.json({ success: data });
+    })
+    .catch((error) => {
+      response.json({ failured: error });
+    });
 });
 
 module.exports = router;
